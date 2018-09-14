d84e16966e2a8e1dca7b7d4b3113dc4b00b77286
xuyifan422 <1135672070@qq.com>
2018-09-14 23:50:42 +0800

    first commit

---
+++ src/main/java/com/xuyifan/dspmanager/DspManagerApplication.java
@@ -0,0 +1,12 @@
+package com.xuyifan.dspmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DspManagerApplication.class, args);
+    }
+}
